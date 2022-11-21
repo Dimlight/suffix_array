49bdc991d188a6c10f2d432f873233e78947e92c
Ekram Hossain <ekramhossain@Ekrams-MacBook-Pro.local>
2022-11-21 18:32:57 -0500

    suffix code

---
+++ optimized.cpp
@@ -0,0 +1,269 @@
+#include<stdio.h>
+#include <time.h> 
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <set>
+#include <cmath>
+#include <cstring>
+
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
+#include <stdlib.h>
+#include <vector>
+#include <iomanip>
+#include <ctime>
+#include <string>
+#include<cstring>
+#include<string.h>
+
+
+#define REP(i,n) for (int i=0;i<n;i++)
+#define Clear(x,with) memset(x,with,sizeof(x))
+
+using namespace std;
+
+
+#define MAXN 400500
+#define MAXL 26
+ 
+int Case, n, pos, m, suffix[MAXN], tmp[MAXN], sum[MAXN], Count[MAXN], Rank[MAXL][MAXN];
+char input[MAXN], temp[MAXN], pat[MAXN], reversest[MAXN][MAXN];
+int occurences[MAXN], revoccurences[MAXN];
+
+string patterns[MAXN]; 
+
+bool checkEqual(const int &u, const int &v)
+{
+    if(!pos) 
+        return input[u] == input[v];
+    if(Rank[pos-1][u] != Rank[pos-1][v]) 
+        return false;
+    int a = u + m < n ? Rank[pos-1][u+m] : -1;
+    int b = v + m < n ? Rank[pos-1][v+m] : -1;
+    return a == b;
+}
+ 
+void updateRank()
+{
+    int i, rnk;
+    REP(i,n) sum[i] = 0;
+    for(i = rnk = 0; i < n; i++) 
+    {
+        suffix[i] = tmp[i];
+        if(i && !checkEqual(suffix[i], suffix[i-1])) 
+        {
+            Rank[pos][suffix[i]] = ++rnk;
+            sum[rnk+1] = sum[rnk];
+        }
+        else 
+            Rank[pos][suffix[i]] = rnk;
+        sum[rnk+1]++;
+    }
+}
+ 
+void bucketSort() 
+{
+    int i;
+    REP(i,n) Count[i] = 0;
+    Clear(tmp,-1);
+    
+    for(i = 0; i < m; i++)
+    {
+        int idx = Rank[pos - 1][n - i - 1];
+        int x = sum[idx];
+        tmp[x + Count[idx]] = n - i - 1;
+        Count[idx]++;
+    }
+    REP(i,n)
+    {
+        int idx = suffix[i] - m;
+        if(idx < 0)
+            continue;
+        idx = Rank[pos-1][idx];
+        int x = sum[idx];
+        tmp[x + Count[idx]] = suffix[i] - m;
+        Count[idx]++;
+    }
+    
+    updateRank();
+    
+    return;
+}
+ 
+bool Compare(const int &a, const int &b)
+{
+    if(input[a]!=input[b]) return input[a]<input[b];
+    return false;
+}
+ 
+void sortSuffixes() 
+{
+    int i;
+    REP(i,n) tmp[i] = i;
+
+    sort(tmp, tmp + n, Compare);
+    
+    pos = 0;
+    updateRank();
+    ++pos;
+    for(m = 1; m < n; m <<= 1) {
+        bucketSort(); // Resources on Bucket sort I read: https://www.scaler.com/topics/data-structures/bucket-sort/ 
+        pos++;
+    }
+    pos--;
+    for(i = 0; i <= pos; i++)
+    {
+        Rank[i][n] = -1;
+    } 
+        
+}
+int occurance = 0; 
+
+string convertToString(char* a)
+{
+    string s(a);
+
+    return s;
+}
+void printsuffix(char *txt, int *suffArr, int n)
+{
+    for(int i=0; i <n ; i++)
+    {   
+        string st = convertToString(txt+suffArr[i]);
+        printf("sufix-> %d %s\n",i,st.c_str());
+        //freq[st]++;
+    }
+}
+char* reverse(char *str) {                       // defining the function
+    static int i = 0;
+    static char rev[100];
+    if(*str)
+    {
+    reverse(str+1);
+    rev[i++] = *str;
+    }
+    return rev;
+}
+
+
+int suffixArraySearch(char *txt,int *suffix, const char *substring, int n)
+{
+    int lo;
+    int hi;
+    int mid;
+    int len;
+    int cmp;
+
+    len = strlen(substring);
+
+    /* invariant: suffix[lo] <= substring < suffix[hi] */
+    lo = 0;
+    hi = n;
+
+    while(lo + 1 < hi) {
+        mid = (lo+hi)/2;
+        cmp = strncmp(txt+ suffix[mid], substring, len);
+
+        if(cmp == 0) {
+            /* we have a winner */
+            /* search backwards and forwards for first and last */
+            for(lo = mid; lo > 0 && strncmp(txt + suffix[lo-1], substring, len) == 0; lo--);
+            for(hi = mid; hi < n && strncmp(txt + suffix[hi+1], substring, len) == 0; hi++);
+
+            return hi - lo + 1;
+        } else if(cmp < 0) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+
+    return 0;
+}
+
+
+int main()
+{
+    // freopen("large.in","r",stdin);
+    // freopen("large.out","w",stdout);
+    //cin >> Case;
+
+    
+    
+    //REP(i,Case)
+    //{
+    cin >> input;
+    
+    n = strlen(input); 
+    
+    int npat;
+    cin >> npat; 
+
+    // compute the suffix tree 
+    sortSuffixes();
+    // print the suffix tree; 
+    // printsuffix(input, suffix,n);
+
+    REP(j,npat)
+    {
+        cin >> patterns[j];
+        strcpy(pat, patterns[j].c_str());
+        occurences[j] = suffixArraySearch(input,suffix,pat,n);
+        // save the reverstring
+        
+        //strcpy(reversest[j],reverse(pat));
+        printf("pattern: %s: -> %d\n",pat,occurences[j]);
+    }
+
+    // strcpy(input,reverse(input));
+
+    // // compute the suffix tree 
+    // sortSuffixes();
+    
+    // REP(j,npat)
+    // {
+    //     strcpy(pat, reversest[j]);
+    //     revoccurences[j] = suffixArraySearch(input,suffix,pat,n);
+        
+    //     printf("pattern: %s: -> %d\n",pat,revoccurences[j]);
+    // }
+    
+    // int res = 0;
+    
+    // for(int i=0,j=npat-1; i<npat ; i++,j--)
+    // {
+    //     res+= occurences[i]*revoccurences[j];
+    // }
+
+    // printf("%d\n",res);
+
+    // compute the suffix tree 
+    // sortSuffixes();
+    // print the suffix tree; 
+    // printsuffix(input, suffix,n);
+    //int res = 0; 
+    // REP(l,npat)
+    // {
+    //     REP(k,npat)
+    //     {
+    //         //cout << pat[l] + pat[k] << endl; 
+    //         string subst = patterns[l] + patterns[k];
+    //         strcpy(pat, subst.c_str());
+    //         int tempres = suffixArraySearch(input,suffix,pat,n);
+    //         res+=tempres;
+    //     }
+    // }
+
+    //printf("%d\n", res);
+    //}
+    
+    
+    return 0;
+}
